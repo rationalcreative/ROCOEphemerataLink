269839bd8188b1b25bb72bc0c75cde1ee69a04ee
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:10 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -4,7 +4,7 @@ Object.entries({
 	ROCOEphemerataLink: '.ROCOEphemerataLink',
 	ROCOEphemerataLinkImage: '.ROCOEphemerataLinkImage',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('ROCOEphemerataLink_Access', function () {
